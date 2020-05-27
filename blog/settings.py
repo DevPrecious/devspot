1ee2098738c3c307e7d5c3e59d0cef53af9285d1
Precious Oladele <51764615+DevPrecious@users.noreply.github.com>
2020-05-27 15:30:12 +0100

    Update settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -118,9 +118,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-django_heroku.settings(locals())
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
